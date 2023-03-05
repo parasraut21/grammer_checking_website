9bfb431e7a1409592827a8b8f41af319196c7bf1
parasraut21 <111653346+parasraut21@users.noreply.github.com>
2023-03-05 13:45:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+ import { HashRouter, Routes, Route } from "react-router-dom";
+
+
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Textform from "./components/Textform";
+import Alert from "./components/Alert";
+import About from "./components/About";
+
+//  import {  Routes , Route } from "react-router-dom";
 
 function App() {
+  const [mode, setmode] = useState("light");
+  const [alert, setalert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setalert({
+      msg: message,
+      type: type,
+    });
+
+    setTimeout(() => {
+      setalert(null);
+    }, 3000);
+  };
+
+  const togglemode = () => {
+    if (mode === "light") {
+      setmode("dark");
+      document.body.style.backgroundColor = "#042743";
+      showAlert("Dark Mode Has Been Enabled", "success");
+      document.title = "Grammar Corrector - Dark Mode";
+    } else {
+      setmode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light Mode Has Been Enabled", "success");
+      document.title = "Grammar Corrector - Light Mode";
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        {/* <Navbar />  */}
+       {/* <Navbar  title="Grammer Corrector"  mode={mode} togglemode={togglemode}/>
+       <Alert alert={alert} />
+    
+      <Textform
+          showAlert={showAlert}
+          heading="Enter your Text to analyze"
+          mode={mode}
+        />  */}
+     
+          <div className="container my-3">
+          {/* <Textform 
+          showAlert={showAlert}
+          heading="Enter your Text to analyze"
+          mode={mode}
+        />   */}
+           <HashRouter > 
+          <Navbar  title="Grammer Corrector"  mode={mode} togglemode={togglemode}/>
+        <Alert alert={alert} />
+         <Routes>
+        <Route path='/' element={<Textform showAlert={showAlert} heading="Enter your Text to analyze" mode={mode}/>}/>
+          <Route path='about' element={<About/>} />
+        </Routes>
+        </HashRouter> 
+      </div> 
+   
+    </>
   );
 }
 
---
+++ src/components/Textform.js
@@ -0,0 +1,127 @@
+import React , {useState} from 'react'
+import { GrammarlyEditorPlugin } from '@grammarly/editor-sdk-react'
+
+
+
+export default function Textform(props) {
+const [text,setText] = useState('');
+//uppercase
+ const handleuptext=()=>{
+   // console.log("Convert To Uppercase Was Clicked");
+     let newText = text.toUpperCase();
+     setText(newText);
+     props.showAlert("Successfully Converted To Upper Case","success");
+ }
+
+ //speak
+ const speak = () => {
+    let msg = new SpeechSynthesisUtterance();
+    msg.text = text;
+    window.speechSynthesis.speak(msg);
+  }
+
+  
+
+
+  // remove extra spaces
+  const handleExtraSpaces = ()=>{
+    let words = text.split(' ');
+    let joinedWords = '';
+    // console.log(words);
+    words.forEach((elem)=>{
+        if(elem[0] !== undefined){
+            joinedWords += elem + " ";
+            console.log(joinedWords);
+        }
+    })
+    setText(joinedWords);
+    props.showAlert("Successfully Removed Extra Spaces","success");
+}
+
+//download text files
+const downloadTxtFile = () => {
+    const element = document.createElement("a");
+    const file = new Blob([text], {
+      type: "text/plain"
+    });
+    element.href = URL.createObjectURL(file);
+    element.download = "myFile.txt";
+    element.click();
+    props.showAlert("Successfully Downloaded The Text File","success");
+}
+
+//copy to clipboard
+const copyText = () => {
+    navigator.clipboard.writeText(text);
+    props.showAlert("Successfully copied the text","success");
+}
+ const handlelotext=()=>{
+    // console.log("Convert To Uppercase Was Clicked");
+      let newText = text.toLowerCase();
+      setText(newText);
+      props.showAlert("Successfully converted to Lower Case","success");
+  }
+ const handleuponchange=(event)=>{
+    // console.log("Uppercase on change");
+     setText(event.target.value)
+ }
+
+
+//clear text 
+const clearttext=()=>{
+  let newText="";
+  setText(newText);
+  props.showAlert("Text Cleared","success");
+}
+
+const countWords = (str) => {
+  let words;
+  if (text === "") {
+
+    words = 0;
+  } else {
+    words = str.trim().split(/\s+/).length;
+  }
+  return words;
+};
+
+
+//text="New text";//wrong way to set a text
+//setText("New Text");
+
+  return (
+      <>
+  <div className="container my-2"  style={{color:props.mode==='dark'?'white':'#042743'}}>
+  <h1>{props.heading}</h1>
+<div class="mb-3">
+<GrammarlyEditorPlugin clientId="client_J3yN5YxZyPjamoYchMARLo">
+ <textarea className="form-control"  value={text} id="mybox"  rows="8" onChange={handleuponchange}  style={{backgroundColor:props.mode==='dark'?'grey':'white' ,
+color:props.mode==='dark'?'white':'#042743'}} ></textarea>
+    </GrammarlyEditorPlugin>
+</div>
+<div class="d-grid gap-2 d-md-block">
+<button type="submit" onClick={speak} className="btn btn-warning mx-2 my-2">Speak</button>
+<button className="btn btn-outline-primary mx-2"  onClick={handleExtraSpaces}  >Remove Extra Spaces</button>
+<button className='btn btn-primary mx-2' onClick={copyText}>Copy To Clipboard</button>
+<button className="btn btn-outline-primary mx-2"  onClick={handleuptext}  >Convert To Upper case</button>
+<button className="btn btn-primary mx-2"  onClick={handlelotext}  >Convert To Lower Case</button>
+<button className="btn btn-outline-primary mx-2"  onClick={clearttext}  >Clear Text</button>
+<button className='btn btn-primary mx-2' onClick={downloadTxtFile}>Download Text file</button>
+
+</div>
+
+  </div>
+     
+  <div className="container my-3" style={{color:props.mode==='dark'?'white':'#042743'}}>
+      <h2>Your Text Summary</h2>
+      <p> <b>{countWords(text)}</b>  <b>Words</b> | <b>{text.length}</b>  <b>Charater</b> </p>
+      <p> <b>{0.008 * countWords(text)}</b>  <b>Minutes Read</b> </p>
+      <h2>Preview</h2>
+      <p><b>{text.length>0?text:"Enter Your Text To Preview"}</b></p>
+
+  </div>
+
+      </>
+   
+  )
+}
